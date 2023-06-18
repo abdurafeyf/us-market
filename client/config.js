5f08d3b9b3e9f64e510dfcb20b774e93015750da
abdurafeyf <92419613+abdurafeyf@users.noreply.github.com>
2023-06-18 22:33:58 +0500

    backend process initiated

---
+++ client/config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+      host: "10.0.1.52",
+      user: "laeeqahmad",
+      password: "SarmaayaDB@User786",
+      database: "laeeqahm_ver2_sarmaaya_pk",
+    },
+    listPerPage: 10,
+};
+
+module.exports = config;
