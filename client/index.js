2d0bc111ed2ba6c748629468f0a32b92ff4f2e5e
abdurafeyf <92419613+abdurafeyf@users.noreply.github.com>
2023-06-18 22:58:33 +0500

    updated index.js file

--- client/index.js
+++ client/index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const programmingLanguagesRouter = require("./routes/programmingLanguages");
+const quoteDataRoute = require("./routes/quoteData");
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+
 app.get("/", (req, res) => {
   res.json({ message: "ok" });
 });
-app.use("/programming-languages", programmingLanguagesRouter);
+
+app.use("/quote-data", quoteDataRoute);
 /* Error handler middleware */
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
