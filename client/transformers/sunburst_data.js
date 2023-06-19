4d295df990bc882d4a540534f85563dd31feadc9
abdurafeyf <92419613+abdurafeyf@users.noreply.github.com>
2023-06-20 00:35:30 +0500

    added some more data in the finalData array

--- client/transformers/sunburst_data.js
+++ client/transformers/sunburst_data.js
@@ -19,14 +19,26 @@ function sunburst_data(data) {
     // Iterate over the groupedData object and create the finalData array
     for (const sector in groupedData) {
         var sector_change = 0.0;
+        var sector_current = 0.0;
+        var sector_marketcap = 0.0;
+        var sector_stock_volume = 0.0;
+        var sector_weightage = 0.0;
         for (const i in groupedData[sector]) {
             sector_change += parseFloat(groupedData[sector][i]['stock_change_p']);
+            sector_current += parseFloat(groupedData[sector][i]['stock_current_price']);
+            sector_marketcap += parseFloat(groupedData[sector][i]['marketcap']);
+            sector_stock_volume += parseFloat(groupedData[sector][i]['stock_volume_m']);
+            sector_weightage += parseFloat(groupedData[sector][i]['weightage']);
         }
         id += 1;
         finalData.push({
-            sector_code: id, 
+            sector_code: id,
             stock_sector_name: sector,
+            sector_current: sector_current,
             sector_change: sector_change,
+            weightage: sector_weightage,
+            marketcap: sector_marketcap,
+            stock_volume: sector_stock_volume,
             stocks: groupedData[sector]
         });
     }
