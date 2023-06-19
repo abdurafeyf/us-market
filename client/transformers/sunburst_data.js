455bd57649d8e22ddef81600533dc76a02e031c2
abdurafeyf <92419613+abdurafeyf@users.noreply.github.com>
2023-06-19 23:59:39 +0500

    shaping the data

--- client/transformers/sunburst_data.js
+++ client/transformers/sunburst_data.js
@@ -6,11 +6,15 @@ function sunburst_data(data) {
     data.forEach((obj) => {
         const sector = obj.stock_sector;
 
+        finalData.push({
+            stock_sector_name: sector,
+            stocks:[]
+        })
+        
         // Check if the sector already exists in the groupedData object
         if (!groupedData.hasOwnProperty(sector)) {
-            groupedData[sector] = [];
+            finalData[sector] = [];
         }
-
         // Push the object to the corresponding sector array
         groupedData[sector].push(obj);
         finalData.push(groupedData);
