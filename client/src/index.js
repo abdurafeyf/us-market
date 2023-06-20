1b43839a2028f5ed2e0adbd5fc6c0af378f1f9e1
abdurafeyf <92419613+abdurafeyf@users.noreply.github.com>
2023-06-21 01:07:47 +0500

    sunburst formed from basic data

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import SunburstAnyChart from "./components/SunburstChart/SunburstAnyChart";
+//import SunburstD3V4 from "./SunburstD3V4"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div className="App">
+      <h1>This is a new chart</h1>
+      <SunburstAnyChart />
+      {/* <div id="chart" /> */}
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
