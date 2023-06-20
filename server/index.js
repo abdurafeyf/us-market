013b7ee7d8ba76ac9d953b90c105dec19311f461
abdurafeyf <92419613+abdurafeyf@users.noreply.github.com>
2023-06-20 09:58:51 +0500

    port changed for backend

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const port = 5000;
 const quoteDataRoute = require("./routes/quoteData");
 
 app.use(express.json());
