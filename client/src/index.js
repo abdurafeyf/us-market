9800192cf214ca4c6fd3631e4791ff01ebd9cda5
abdurafeyf <92419613+abdurafeyf@users.noreply.github.com>
2023-06-21 09:56:53 +0500

    MVP for Sunburst D3 Version 4.0

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SunburstAnyChart from "./components/SunburstChart/SunburstAnyChart";
-//import SunburstD3V4 from "./SunburstD3V4"
+// import SunburstAnyChart from "./components/SunburstChart/SunburstAnyChart";
+import SunburstD3V4 from "./components/SunburstChart/SunburstD3V4"
 
 function App() {
   return (
     <div className="App">
       <h1>Sunburst Chart</h1>
-      <SunburstAnyChart />
+      {/* <SunburstAnyChart /> */}
       {/* <div id="chart" /> */}
+      <SunburstD3V4/>
     </div>
   );
 }
