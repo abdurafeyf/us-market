fbb8f1c13615a24f3e9c3e28c68989253fe3653b
abdurafeyf <92419613+abdurafeyf@users.noreply.github.com>
2023-06-21 23:36:31 +0500

    sunburst working properly on data

--- client/src/components/SunburstChart/SunburstD3V4.js
+++ client/src/components/SunburstChart/SunburstD3V4.js
@@ -1,42 +1,68 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Sunburst from "react-zoomable-sunburst-d3-v4";
 
-// const data = {
-//   name: "TOPICS",
-//   children: [
-//     {
-//       name: "Topic A",
-//       children: [{ name: "Sub A1", size: 4 }, { name: "Sub A2", size: 4 }]
-//     },
-//     {
-//       name: "Topic B",
-//       children: [
-//         { name: "Sub B1", size: 3 },
-//         { name: "Sub B2", size: 3 },
-//         {
-//           name: "Sub B3",
-//           size: 3
-//         }
-//       ]
-//     },
-//     {
-//       name: "Topic C",
-//       children: [{ name: "Sub A1", size: 4 }, { name: "Sub A2", size: 4 }]
-//     }
-//   ]
-// };
-
 const baseUrl = "http://127.0.0.1:5000/quote-data";
-
+let data = {
+    name: "TOPICS",
+    children: [
+      {
+        name: "Topic A",
+        children: [{ name: "Sub A1", size: 4 }, { name: "Sub A2", size: 4 }]
+      },
+      {
+        name: "Topic B",
+        children: [
+          { name: "Sub B1", size: 3 },
+          { name: "Sub B2", size: 3 },
+          {
+            name: "Sub B3",
+            size: 3
+          }
+        ]
+      },
+      {
+        name: "Topic C",
+        children: [{ name: "Sub A1", size: 4 }, { name: "Sub A2", size: 4 }]
+      }
+    ]
+  };
+  console.log('main data', data);
 const SunburstD3V4 = () => {
   const onSelect = (event) => {
     // console.log(event);
   };
+
+  const transformData = (data) => {
+    const transformedData = {
+      name: "NASDAQ",
+      children: [],
+    };
+
+    data.forEach((sector) => {
+      const sectorNode = {
+        name: sector.stock_sector_name,
+        children: [],
+      };
+
+      sector.stocks.forEach((stock) => {
+        sectorNode.children.push({
+          name: stock.stock_symbol,
+          size: stock.weightage,
+        });
+      });
+
+      transformedData.children.push(sectorNode);
+    });
+
+    return transformedData;
+  };
+
   const [post, setPost] = useState(null);
   useEffect(() => {
     axios.get(baseUrl).then((response) => {
-        setPost(response.data);
+      const transformedData = transformData(response.data.data);
+      setPost(transformedData);
     });
   }, []);
 
