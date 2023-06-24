23172d32749711b2a02f1a9f4d12634c7ec917c9
abdurafeyf <92419613+abdurafeyf@users.noreply.github.com>
2023-06-24 13:22:32 +0500

    dotenv module added to export credentials

--- server/config.js
+++ server/config.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const config = {
     db: {
-      host: "db4free.net",
-      user: "abdurafeyf",
-      password: "abdurafeyf",
-      database: "usmarket",
+      host: process.env.DB4FREENET_HOST,
+      user: process.env.DB4FREENET_USER_NAME,
+      password: process.env.DB4FREENET_PASSWORD,
+      database: process.env.DB4FREENET_DATABASE,
     },
     listPerPage: 10,
 };
