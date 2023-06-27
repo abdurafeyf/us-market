c3a7002d6dae3a7478d0330dc3e1e62734d1e741
abdurafeyf <92419613+abdurafeyf@users.noreply.github.com>
2023-06-27 20:39:22 +0500

    added a temp navbar

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import SunburstAnyChart from "./components/SunburstChart/SunburstAnyChart";
-// import SunburstD3V4 from "./components/SunburstChart/SunburstD3V4";
-// import { SunburstChartTS } from "./components/SunburstChart/SunburstChartTS";
-import ComboChartLW from "./components/ComboChart/ComboChartLW";
+import './App.css';
+import NavBar from "./components/NavBar/NavBar";
 function App() {
   return (
-    <div className="App">
-      <h1>Combo Chart</h1>
-      {/* <SunburstAnyChart /> */}
-      {/* <div id="chart" /> */}
-      {/* <SunburstD3V4/> */}
-      {/* <SunburstChartTS/> */}
-      <ComboChartLW/>
-    </div>
+    <NavBar/>
   );
 }
 
