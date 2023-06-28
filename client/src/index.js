9904bedc595eb983228c5d88591c0d288ca73ce1
abdurafeyf <92419613+abdurafeyf@users.noreply.github.com>
2023-06-28 15:23:54 +0500

    removed navbar for now

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
-import NavBar from "./components/NavBar/NavBar";
-// import ComboChartLW from "./components/ComboChart/ComboChartLW";
-// import SunburstD3V4 from "./components/SunburstChart/SunburstD3V4";
+import ComboChartLW from "./components/ComboChart/ComboChartLW";
+import SunburstD3V4 from "./components/SunburstChart/SunburstD3V4";
 function App() {
   return (
     <div>
-      <div>
-        <NavBar/>
-        {/* <SunburstD3V4/> */}
-        {/* <ComboChartLW/> */}
+      <div>        
+        <SunburstD3V4/>
+        <ComboChartLW/>
       </div>
     </div>
   );
