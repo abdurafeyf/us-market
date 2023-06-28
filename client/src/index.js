b3e4d13e91c99d15d346ed4d5e1cdcc454b1232c
abdurafeyf <92419613+abdurafeyf@users.noreply.github.com>
2023-06-28 20:09:53 +0500

    removed navigation bar

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom";
 import './index.css';
 import ComboChartLW from "./components/ComboChart/ComboChartLW";
 import SunburstD3V4 from "./components/SunburstChart/SunburstD3V4";
+// import NavBar from "./components/NavBar/NavBar";
 function App() {
   return (
     <div>
-      <div>        
-        <SunburstD3V4/>
-        <ComboChartLW/>
-      </div>
+      <SunburstD3V4/>
+      <ComboChartLW/>
     </div>
   );
 }
